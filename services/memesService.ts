ef4ab696590a55de8fec6ccd25b96504de1afa3c
Elian <elian_fl@hotmail.com>
2025-12-27 02:27:58 +0100

    feat supabase

--- services/memesService.ts
+++ services/memesService.ts
@@ -1,26 +1,28 @@
-// TODO: redo wiht supabase
+import { supabase } from "@/lib/supabase";
 import { FILTER_CATEGORIES } from "@/types/memes.types";
-import { MEMES_DATA } from "@/utils/memes.utils";
 
 export const fetchMemes = async (categoryId: string, search: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 800)); // Simulación UX
+  let query = supabase
+    .from("memes")
+    .select("*")
+    .order("created_at", { ascending: false });
 
-  let filtered = [...MEMES_DATA];
-
-  // Buscamos la categoría por ID (el que viene de la URL)
   const category = FILTER_CATEGORIES.find((c) => c.id === categoryId);
 
-  // Si encontramos la categoría y tiene un tipo asociado, filtramos
   if (category && category.type) {
-    filtered = filtered.filter((meme) => meme.type === category.type);
+    query = query.eq("type", category.type);
   }
 
   if (search) {
-    const query = search.toLowerCase();
-    filtered = filtered.filter((meme) =>
-      meme.title.toLowerCase().includes(query)
-    );
+    query = query.ilike("title", `%${search}%`);
   }
+  const { data, error } = await query;
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const filtered = data || [];
 
   return filtered;
 };
